e824dd752304ca0d3924b5e52e08c824177e3c9a
kelzerock <76631388+kelzerock@users.noreply.github.com>
2021-01-08 02:02:07 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,99 @@
+function monday(){
+  var elems=document.getElementsByClassName('mn');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='none';
+
+  var elems=document.getElementsByClassName('monday');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='';
+
+  var elems=document.getElementsByClassName('d_8');
+  for(var i=0; i<elems.length; i++)elems[i].style.background='#7492ec';
+
+  var elems=document.
+  getElementsByClassName('d_1');
+  for(var i=0; i<elems.length; i++)
+  elems[i].style.background='#4865bd';
+}
+function tuesday(){
+  var elems=document.getElementsByClassName('tu');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='none';
+  var elems=document.getElementsByClassName('tuesday');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='';
+
+  var elems=document.getElementsByClassName('d_8');
+  for(var i=0; i<elems.length; i++)elems[i].style.background='#7492ec';
+
+  var elems=document.
+  getElementsByClassName('d_2');
+  for(var i=0; i<elems.length; i++)
+  elems[i].style.background='#4865bd';
+}
+function wednesday(){
+  var elems=document.getElementsByClassName('we');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='none';
+  var elems=document.getElementsByClassName('wednesday');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='';
+  
+  var elems=document.getElementsByClassName('d_8');
+  for(var i=0; i<elems.length; i++)elems[i].style.background='#7492ec';
+
+  var elems=document.
+  getElementsByClassName('d_3');
+  for(var i=0; i<elems.length; i++)
+  elems[i].style.background='#4865bd';
+}
+function thursday(){
+  var elems=document.getElementsByClassName('th');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='none';
+  var elems=document.getElementsByClassName('thursday');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='';
+  
+  var elems=document.getElementsByClassName('d_8');
+  for(var i=0; i<elems.length; i++)elems[i].style.background='#7492ec';
+
+  var elems=document.
+  getElementsByClassName('d_4');
+  for(var i=0; i<elems.length; i++)
+  elems[i].style.background='#4865bd';
+}
+function friday(){
+  var elems=document.getElementsByClassName('fr');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='none';
+  var elems=document.getElementsByClassName('friday');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='';
+  
+  var elems=document.getElementsByClassName('d_8');
+  for(var i=0; i<elems.length; i++)elems[i].style.background='#7492ec';
+
+  var elems=document.
+  getElementsByClassName('d_5');
+  for(var i=0; i<elems.length; i++)
+  elems[i].style.background='#4865bd';
+}
+function saturday(){
+  var elems=document.getElementsByClassName('sa');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='none';
+  var elems=document.getElementsByClassName('saturday');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='';
+  
+  var elems=document.getElementsByClassName('d_8');
+  for(var i=0; i<elems.length; i++)elems[i].style.background='#7492ec';
+
+  var elems=document.
+  getElementsByClassName('d_6');
+  for(var i=0; i<elems.length; i++)
+  elems[i].style.background='#4865bd';
+}
+function sunday(){
+  var elems=document.getElementsByClassName('su');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='none';
+  var elems=document.getElementsByClassName('sunday');
+  for(var i=0; i<elems.length; i++)elems[i].style.display='';
+  
+  var elems=document.getElementsByClassName('d_8');
+  for(var i=0; i<elems.length; i++)elems[i].style.background='#7492ec';
+
+  var elems=document.
+  getElementsByClassName('d_7');
+  for(var i=0; i<elems.length; i++)
+  elems[i].style.background='#4865bd';
+}
